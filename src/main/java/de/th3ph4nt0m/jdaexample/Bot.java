d5a1ed702c1429e91f2a4d3c4a4438996eddb69a
Henrik S <Development@th3ph4nt0m.de>
2020-11-08 20:09:09 +0100

    update copyright

--- src/main/java/de/th3ph4nt0m/jdaexample/Bot.java
+++ src/main/java/de/th3ph4nt0m/jdaexample/Bot.java
@@ -1,3 +1,11 @@
+/*
+ * Bot.java is part of the JDA-Example project
+ *
+ * The JDA-Example is licensed under The Unlicense.
+ *
+ * Copyright (c) 2020 Henrik Steffens
+ */
+
 package de.th3ph4nt0m.jdaexample;
 
 public class Bot
