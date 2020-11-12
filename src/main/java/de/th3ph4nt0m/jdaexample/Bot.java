03539460f908ab10dae0e26b9710f4c13e16eedb
Henrik S <Development@th3ph4nt0m.de>
2020-11-12 17:56:25 +0100

    created Bot.java

--- src/main/java/de/th3ph4nt0m/jdaexample/Bot.java
+++ src/main/java/de/th3ph4nt0m/jdaexample/Bot.java
@@ -8,6 +8,41 @@
 
 package de.th3ph4nt0m.jdaexample;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+
+import javax.security.auth.login.LoginException;
+
 public class Bot
 {
+    private static Bot instance;
+    private JDA jda;
+
+
+    public Bot()
+    {
+        instance = this;
+        try {
+            this.jda = JDABuilder.createDefault("token")                //Creates a JDABuilder with recommended default settings
+                    .setAutoReconnect(true)                             //Sets whether or not JDA should try to reconnect if a connection-error is encountered
+                    .setStatus(OnlineStatus.ONLINE)                     //Sets the OnlineStatus our connection will display
+                    .setActivity(Activity.listening("to everyone"))     //Sets the Activity for our session
+                    .build();                                           //Builds a new JDA instance and uses the provided token to start the login process
+        } catch (LoginException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static Bot getInstance()
+    {
+        return instance;
+    }
+
+    public JDA getJda()
+    {
+        return jda;
+    }
 }
